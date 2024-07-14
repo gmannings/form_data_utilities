b2853db15c56c34f06b6a706cd390f3437e6900b
Gavin Mannings <g.mannings@hotmail.co.uk>
2024-07-14 15:25:12 +0100

    Added container element.

---
+++ src/FormElement/Container.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\form_data_utilities\FormElement;
+
+use Drupal\form_data_utilities\ElementType;
+use Drupal\form_data_utilities\FormBuilder;
+use Drupal\form_data_utilities\FormElement;
+use Drupal\form_data_utilities\FormElementInterface;
+use Drupal\form_data_utilities\FormElementTrait\Attributes;
+use Drupal\form_data_utilities\FormElementTrait\Value;
+
+class Container extends FormElement implements FormElementInterface {
+
+  use Attributes;
+
+  public function __construct(FormBuilder $formBuilder, ?FormElementInterface $parent = NULL) {
+    parent::__construct($formBuilder, $parent);
+    $this->type = ElementType::CONTAINER;
+  }
+
+}
---
+++ test/Drupal/form_data_utilties/FormElement/ContainerTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Drupal\form_data_utilities\FormElement;
+
+use Drupal\form_data_utilities\FormBuilder;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+class ContainerTest extends TestCase {
+
+  protected Container $element;
+
+  protected FormBuilder $mockFormBuilder;
+
+  #[Test] public function testGetRenderArray(): void {
+    $this->element = new Container($this->mockFormBuilder);
+    $this->assertEquals(
+      [
+        '#type' => 'container',
+      ],
+      $this->element->getRenderArray()
+    );
+  }
+
+  #[Test] public function testMethodsExist(): void {
+    $this->element = new Container($this->mockFormBuilder);
+    $this->assertTrue(method_exists($this->element, 'setAttributeId'));
+    $this->assertTrue(method_exists($this->element, 'setAttributeClass'));
+  }
+
+  #[Test] public function testDataMethods(): void {
+    $this->element = new Container($this->mockFormBuilder);
+    $this->element
+      ->setAttributeId('test')
+      ->setAttributeClass(['test']);
+
+    $this->assertEquals(
+      [
+        '#type' => 'container',
+        '#attributes' => [
+          'id' => 'test',
+          'class' => ['test'],
+        ]
+      ],
+      $this->element->getRenderArray()
+    );
+  }
+
+  protected function setUp(): void {
+    parent::setUp();
+    $this->mockFormBuilder = $this->createMock(FormBuilder::class);
+  }
+
+}
